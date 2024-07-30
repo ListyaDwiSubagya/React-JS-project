c28a0337cb2589dd94d511130f4c8844da448dc8
ListyaDwiSubagya <listyodwi062@gmail.com>
2024-07-31 00:06:14 +0700

    joke generator

---
+++ joke generator/src/App.jsx
@@ -0,0 +1,12 @@
+import Joke from "./components/Joke/Joke";
+
+export default function App() {
+    return (
+        <div className="App">
+            <h1>Joke Generator Using React and Joke API</h1>
+            <Joke/>
+        </div>
+    );
+}
+
+ 
