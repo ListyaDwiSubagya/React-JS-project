b9edea6ee9dee431083a81c3ac6f7b8c8dec3b86
ListyaDwiSubagya <listyodwi062@gmail.com>
2024-08-05 13:10:48 +0700

    ui

--- gemini clone/src/App.jsx
+++ gemini clone/src/App.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import Sidebar from './Components/Sidebar/Sidebar'
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import Sidebar from './Components/Sidebar/Sidebar';
+import Main from './Components/Main/Main';
+import './index.css';
 
 const App = () => {
   return (
-    <div>
-      <Sidebar/>
+    <div className="app-container">
+      <Sidebar />
+      <Main />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
