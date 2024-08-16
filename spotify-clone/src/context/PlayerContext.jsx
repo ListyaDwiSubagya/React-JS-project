a450c47752179e5c891b24022dc02a0be8d239cf
ListyaDwiSubagya <listyodwi062@gmail.com>
2024-08-16 17:01:22 +0700

    spotify clone

---
+++ spotify-clone/src/context/PlayerContext.jsx
@@ -0,0 +1,28 @@
+import { createContext, useRef, useState } from "react";
+import { songsData } from "../assets/assets";
+
+export const PlayerContext = createContext();
+
+const PlayerContextProvider = (props) => {
+
+    const audioRef = useRef();
+    const seekBg = useRef();
+    const seekBar = useRef();
+
+    const [track, setTrack] = useState(songsData[0]);
+    
+    const contextValue = {
+        audioRef,
+        seekBar,
+        seekBg
+    }
+
+    return (
+        <PlayerContext.Provider value={contextValue}>
+            {props.children}
+        </PlayerContext.Provider>
+    )
+
+}
+
+export default PlayerContextProvider;
