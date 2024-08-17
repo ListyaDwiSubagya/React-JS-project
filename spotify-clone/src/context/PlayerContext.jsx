da2bf5b2d0968298106b80bc397f66c8856d53a6
ListyaDwiSubagya <listyodwi062@gmail.com>
2024-08-17 23:04:37 +0700

    spotify clone

--- spotify-clone/src/context/PlayerContext.jsx
+++ spotify-clone/src/context/PlayerContext.jsx
@@ -10,11 +10,40 @@ const PlayerContextProvider = (props) => {
     const seekBar = useRef();
 
     const [track, setTrack] = useState(songsData[0]);
+    const [playStatus, setPlayStatus] = useState(false);
+    const [time, setTime] = useState({
+        currentTime:{
+            second:0,
+            minute:0
+        },
+        totalTime:{
+            second:0,
+            minute:0
+        }
+    })
+
+    const play = () => {
+        audioRef.current.play();
+        setPlayStatus(true)
+    }
+
+    const pause = () => {
+        audioRef.current.pause();
+        setPlayStatus(false);
+    }
     
     const contextValue = {
         audioRef,
         seekBar,
-        seekBg
+        seekBg,
+        track,
+        setTrack,
+        playStatus,
+        setPlayStatus,
+        time,
+        setTime,
+        play,
+        pause
     }
 
     return (
