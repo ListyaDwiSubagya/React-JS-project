b2fd19c44c64602485a152c83d04b15e92e8205b
ListyaDwiSubagya <listyodwi062@gmail.com>
2024-12-17 15:59:12 +0700

    logic

--- Authentication System/client/vite-project/src/pages/Login.jsx
+++ Authentication System/client/vite-project/src/pages/Login.jsx
@@ -1,23 +1,90 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { assets } from '../assets/assets'
+import {data, useNavigate} from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const Login = () => {
 
+    const navigate = useNavigate()
+
+    const {backendUrl, setIsLoggedIn, getUserData} = useContext(AppContext)
+
     const [state, setState] = useState('Sign up')
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const onSubmitHandler = async (e) => {
+
+        try {
+            e.preventDefault();
+
+            axios.defaults.withCredentials = true
+
+            if (state === "Sign up") {
+                const {data} = await axios.post(backendUrl + '/api/auth/register', {name, email, password})
+            
+                if (data.success) {
+                    setIsLoggedIn(true)
+                    getUserData()
+                    navigate('/')
+                } else {
+                    toast.error(data.message)
+                }
+            } else {
+                const {data} = await axios.post(backendUrl + '/api/auth/login', {email, password})
+            
+                if (data.success) {
+                    setIsLoggedIn(true)
+                    getUserData()
+                    navigate('/')
+                } else {
+                    toast.error(data.message)
+                }
+            }
+            
+        } catch (error) {
+            toast.error(error.message)
+        }
+
+    }
 
   return (
     <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-tr from-blue-200 to-purple-400'>
-        <img src={assets.logo} alt="" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer'/>
-        <div>
-            <h2>{state === 'Sign up' ? 'Create Account' : 'Login '}</h2>
-            <p>{state === 'Sign up' ? 'Create your account' : 'Login to your account!'}</p>
+        <img onClick={() => navigate('/')} src={assets.logo} alt="" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer'/>
+        <div className='bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-indigo-300 text-sm'>
+            <h2 className='text-3xl font-semibold text-white text-center mb-3'>{state === 'Sign up' ? 'Create Account' : 'Login '}</h2>
+            <p className='text-center text-sm mb-6'>{state === 'Sign up' ? 'Create your account' : 'Login to your account!'}</p>
 
-            <form action="">
-                <div>
+            <form onSubmit={onSubmitHandler} action="">
+                {state === 'Sign up' && (
+                <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
                     <img src={assets.person_icon} alt="" />
-                    <input type="text" placeholder='Full Name' required/>
+                    <input onChange={e => setName(e.target.value)} value={name} className='outline-none bg-transparent' type="text" placeholder='Full Name' required/>
                 </div>
+                )}            
+                <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
+                    <img src={assets.mail_icon} alt="" />
+                    <input onChange={e => setEmail(e.target.value)} value={email} className='outline-none bg-transparent' type="email" placeholder='Email id' required/>
+                </div>
+                <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
+                    <img src={assets.lock_icon} alt="" />
+                    <input onChange={e => setPassword(e.target.value)} value={password} className='outline-none bg-transparent' type="password" placeholder='Password' required/>
+                </div>
+
+                <p onClick={() => navigate('/reset-password')} className='mb-4 text-indigo-500 cursor-pointer'>Forgot password?</p>
+                <button className='w-full py-2.5 rounded-full bg-gradient-to-r from-indigo-500 to bg-indigo-900 text-white font-medium'>{state}</button>
+
             </form>
+
+            {state === 'Sign up' ? (
+                <p className='text-gray-400 text-center text-xs mt-4'>Already have an account? {' '} <span onClick={() => setState('Login')} className='underline text-blue-400 cursor-pointer '>Login here</span></p>
+            ) : (
+
+                <p className='text-gray-400 text-center text-xs mt-4'>Don't have an account? {' '} <span onClick={() => setState('Sign up')} className='underline text-blue-400 cursor-pointer '>Sign up</span></p>
+            )}
       
         </div>
     </div>
